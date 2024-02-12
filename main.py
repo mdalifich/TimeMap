771287b9f0390be6bcb98f9f38edad649dcb9cf1
mdalifich <vasjapupkin.2020@mail.ru>
2024-02-12 19:01:14 +0300

    BigProjectForYandexMapVersion0.1

--- main.py
+++ main.py
@@ -19,9 +19,38 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
 
+        key = pygame.key.get_pressed()
+        if key[pygame.K_UP]:
+            MMM.update(0, -1)
+            print('Up')
+        elif key[pygame.K_DOWN]:
+            MMM.update(0, 1)
+            print('Down')
+        elif key[pygame.K_RIGHT]:
+            MMM.update(1, 0)
+            print('Right')
+        elif key[pygame.K_LEFT]:
+            MMM.update(-1, 0)
+            print('Left')
+        elif key[pygame.K_1]:
+            MMM.zooming(1)
+        elif key[pygame.K_2]:
+            MMM.zooming(2)
+        elif key[pygame.K_3]:
+            MMM.zooming(3)
+        elif key[pygame.K_4]:
+            MMM.zooming(4)
+        elif key[pygame.K_5]:
+            MMM.zooming(5)
+        elif key[pygame.K_6]:
+            MMM.zooming(6)
+        elif key[pygame.K_7]:
+            MMM.zooming(7)
+        elif key[pygame.K_8]:
+            MMM.zooming(8)
+
+
         MMM.draw()
         pygame.display.update()
         pygame.display.flip()
     pygame.quit()
-
-response = requests.get("https://static-maps.yandex.ru/1.x/?ll=135.695439%2C-26.781760&l=map&z=4")
--- maps.py
+++ maps.py
@@ -12,13 +12,31 @@ class Map(pygame.sprite.Sprite):
     def __init__(self, x, y, screen):
         super().__init__()
         self.x, self.y = x, y
-        self.response = requests.get("https://static-maps.yandex.ru/1.x/?ll=135.695439%2C-26.781760&l=map&z=4")
-        print(self.response.content)
+        self.WorldX, self.WorldY = 135.6, 26.7
+        self.plusMetka = f'&pt=0%2C0'
+        self.t = 'https://static-maps.yandex.ru/1.x/?'
+        self.zoom = 4
+        self.HTTPS = f"{self.t}ll={str(self.WorldX)}%2C-{str(self.WorldY)}&l=map&z=4"
+        self.response = requests.get(self.HTTPS)
         self.image = pygame.image.load(BytesIO(self.response.content))
-
-
         self.image = pygame.transform.scale(self.image, (600, 450))
         self.screen = screen
+
+    def Point(self, x, y):
+        self.plusMetka += f'~{x}%2C{y}'
+        self.HTTPS += self.plusMetka
+        self.image = pygame.image.load(BytesIO(self.response.content))
+
+    def zooming(self, zoom):
+        self.zoom = zoom
+        self.update(0, 0)
+
+    def update(self, plusX, plusY):
+        self.WorldX += plusX
+        self.WorldY += plusY
+        self.HTTPS = self.t + f"ll={str(self.WorldX)}%2C-{str(self.WorldY)}&l=map&z={self.zoom}" + self.plusMetka
+        self.response = requests.get(self.HTTPS)
+        self.image = pygame.image.load(BytesIO(self.response.content))
 
     def draw(self):
         self.screen.blit(self.image, (self.x, self.y))
